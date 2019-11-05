67b9179812e3677e07ea2dcdb61628f22c5bd1d3
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-05 16:44:42 +0000

    created a delete endpoint to delete entries

--- cars/carsInfoRouter.js
+++ cars/carsInfoRouter.js
@@ -17,37 +17,65 @@ carsInfoRouter.get("/", (req, res) => {
     });
 });
 
-
 carsInfoRouter.get("/:id", async (req, res) => {
-    try {
-      const car = await db
-        .select("*")
-        .where({ car_id: req.params.id })
-        .from("cars");
-      res.status(200).json({
-        car
-      });
-    } catch (error) {
+  try {
+    const car = await db
+      .select("*")
+      .where({ car_id: req.params.id })
+      .from("cars");
+    res.status(200).json({
+      car
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+carsInfoRouter.post("/", async (req, res) => {
+  try {
+    const newEntry = await db
+      .select("*")
+      .from("cars")
+      .insert(req.body)
+      .where({ car_id: req.params.id });
+
+    res.json("New entry got created with an id of " + newEntry[0]);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+carsInfoRouter.put("/:id", (req, res) => {
+  db.select("*")
+    .from("cars")
+    .where({ car_id: req.params.id })
+    .update(req.body)
+    .then(updated => {
+     res.status(200).json(
+      updated + "car(s) information edited succesfully"
+     ) 
+    })
+    .catch(error => {
       res.status(500).json({
-        message: error.message
+        messsage: error.message
       });
-    }
-  });
+    });
+});
 
-
-  carsInfoRouter.post("/", async (req, res) => {
-    try {
-      const newEntry = await db
-        .select("*")
-        .from("cars")
-        .insert(req.body)
-        .where({ car_id: req.params.id });
-  
-      res.json("New entry got created with an id of " + newEntry[0]);
-    } catch (error) {
+carsInfoRouter.delete("/:id", (req, res) => {
+  db.select("*")
+    .from("cars")
+    .where({ car_id: req.params.id })
+    .del()
+    .then(deletedCount => {
+      res.json(deletedCount + " entry got deleted!!");
+    })
+    .catch(error => {
       res.status(500).json({ message: error.message });
-    }
-  });
+    });
+});
 
-  
+
 module.exports = carsInfoRouter;
