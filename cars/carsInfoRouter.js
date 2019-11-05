58e800513bad91384e4026672c85f315fba21614
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-05 15:46:46 +0000

    created a get endpoint to fetch cars with specific ids

--- cars/carsInfoRouter.js
+++ cars/carsInfoRouter.js
@@ -17,4 +17,20 @@ carsInfoRouter.get("/", (req, res) => {
     });
 });
 
+
+carsInfoRouter.get("/:id", async (req, res) => {
+    try {
+      const car = await db
+        .select("*")
+        .where({ car_id: req.params.id })
+        .from("cars");
+      res.status(200).json({
+        car
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message
+      });
+    }
+  });
 module.exports = carsInfoRouter;
