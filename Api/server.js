82fa69d53e2d3092f1d01432de7b33de909538c0
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-05 13:35:45 +0000

    set up carsInfoRouter

--- Api/server.js
+++ Api/server.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const helmet =require('helmet');
+const helmet = require("helmet");
+
+const carsInfoRouter = require("../cars/carsInfoRouter");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use("/api/cars", carsInfoRouter);
 
-module.exports = server ;
+server.get("/", (req, res) => {
+  res.send('<h2> doing something right </h2>');
+});
+module.exports = server;
---
+++ cars/carsInfoRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const db = require("../data/dbConfig");
+
+const carsInfoRouter = express.Router();
+
+
+module.exports = carsInfoRouter
