985a1b67cd1168e42baff5d1e46ecc255a3d2f93
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-05 15:26:58 +0000

    created a get endpoint to fetch cars

--- cars/carsInfoRouter.js
+++ cars/carsInfoRouter.js
@@ -4,5 +4,17 @@ const db = require("../data/dbConfig");
 
 const carsInfoRouter = express.Router();
 
+carsInfoRouter.get("/", (req, res) => {
+  db.select("*")
+    .from("cars")
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error.message
+      });
+    });
+});
 
-module.exports = carsInfoRouter
+module.exports = carsInfoRouter;
