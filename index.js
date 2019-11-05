921162a2d2bf8160a742d5467a4a639c56d01487
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-05 17:36:55 +0000

    set up provisioning

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const server = require("./Api/server.js");
 
-const port = process.env.PORT || 3200;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`server is listening on port ${port}...`);
