1a7777d06f745cf12a057d266bb8aae3bd24eebb
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-05 13:15:34 +0000

    set up dbConfig and wrapped it with knex

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/cars.db3'
+      filename: './data/cars-dealer.db3'
     },
     useNullAsDefault: true,
     migrations: {
